27e0b2a30d5e09b36005e33f0a438addff6ba38d
zlhuey14 <zlhuey14@gmail.com>
2023-10-04 16:11:49 -0500

    html file for the new route.

--- main.py
+++ main.py
@@ -5,8 +5,3 @@ app = create_app()
 if __name__ == '__main__':
     app.run(debug=True)
     # debug=True just means that if a change is made to the code, the web server will automatically re-run
-
-
-#from zach
-
-#from casey
