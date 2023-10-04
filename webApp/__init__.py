c080a45c9f3c3d96ef39010e1400dd8d76694e56
zlhuey14 <zlhuey14@gmail.com>
2023-10-04 11:29:05 -0500

    Still working on the database. Added a test user to be able to login. Having issues with 'login_user(user)' in the auth.py file so I commented it out for now to investigate further.

--- webApp/__init__.py
+++ webApp/__init__.py
@@ -1,12 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from os import path
 
 db = SQLAlchemy()
 GTA_DB = "database.db"
 
-
 def create_app():
-    app = Flask(__name__)
+    app = Flask('webApp')
     app.config['SECRET_KEY'] = 'jkljkljkljkl'
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{GTA_DB}'
     db.init_app(app)
@@ -17,4 +17,17 @@ def create_app():
     app.register_blueprint(auth, url_prefix='/')
     app.register_blueprint(views, url_prefix='/')
 
+    from .tables import User
+
+    with app.app_context():
+        db.drop_all()
+        db.create_all()
+        test_email = 'cs451r@umsystem.edu'
+        test_pass = '12345'
+
+        test_user = User(email=test_email, password=test_pass)
+        db.session.add(test_user)
+        db.session.commit()
+
     return app
+
--- webApp/auth.py
+++ webApp/auth.py
@@ -1,15 +1,28 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, flash, url_for, redirect
+from flask_login import login_user
 
+from . import db
+from .tables import User
 # Blueprints have a bunch of roots and URLs stored inside of it
 # It is a way for us to separate our app out
 
 auth = Blueprint('auth', __name__)
 
-
 @auth.route('/', methods=['GET', 'POST'])
 def login():
     data = request.form
     print(data)
+    if request.method == 'POST':
+        email = request.form.get('username')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if user.password == password:
+                flash('Login successful', category='success')
+                #login_user(user)
+                return redirect(url_for('views.dashboard'))
+
     return render_template("login.html")
 
 
