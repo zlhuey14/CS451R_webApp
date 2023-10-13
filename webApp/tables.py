ef6d949b340c434054de6f1362a76b8c855ce186
zlhuey14 <zlhuey14@gmail.com>
2023-10-13 14:23:23 -0500

    Zach: Started DB table for the GTA Application. Waiting for the frontend to finish for it.

--- webApp/tables.py
+++ webApp/tables.py
@@ -4,3 +4,11 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(200), unique=True)
     password = db.Column(db.String(200))
+
+class GTAApplication(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    #
+    # Fields from the GTA Application will go here.
+    #
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
