e6783459b59e9a6982160829e4ad71e88b16cfa1
caseycook90 <cdcdrh@umsystem.edu>
2023-10-16 14:23:35 -0500

    #from casey - changes made to application page

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ from webApp import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8080)
+    app.run(debug=True)
     # debug=True just means that if a change is made to the code, the web server will automatically re-run
